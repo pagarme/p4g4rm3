eb7344b00bfb35170c20f72f677a9a2c7ca4353d
Dyego Lima <dyego9444@gmail.com>
2018-11-29 17:20:17 -0200

    calculate score using zxcvbn lib

---
+++ test/unit/calculate-password-score.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import p4g4rm3 from '../../.'
+
+test('Too guessable password zxcvbn calculation', t => {
+  const passwordResult = p4g4rm3.calculatePasswordScore('weakpass')
+  t.is(passwordResult.guesses, 161158)
+  t.is(passwordResult.score, 1)
+});
+
+test('Very guessable password zxcvbn calculation', t => {
+  const passwordResult = p4g4rm3.calculatePasswordScore('weakpass12')
+  t.is(passwordResult.guesses, 19063342)
+  t.is(passwordResult.score, 2)
+});
+
+test('Safely unguessable password zxcvbn calculation', t => {
+  const passwordResult = p4g4rm3.calculatePasswordScore('kInd 0k3y')
+  t.is(passwordResult.guesses, 118410000)
+  t.is(passwordResult.score, 3)
+});
+
+test('Very unguessable password zxcvbn calculation', t => {
+  const passwordResult = p4g4rm3.calculatePasswordScore('He!! 0f A p@A55w0rd')
+  t.is(passwordResult.guesses, 48864000000010000)
+  t.is(passwordResult.score, 4)
+});
