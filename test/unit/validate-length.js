dba3b4adaa51fd7b872cb672fc97671414a3912f
Dyego Lima <dyego9444@gmail.com>
2018-11-29 18:51:17 -0200

    validate password maximum length

--- test/unit/validate-length.js
+++ test/unit/validate-length.js
@@ -1,12 +1,25 @@
-import test from 'ava';
+import test from 'ava'
 import p4g4rm3 from '../../.'
 
-test('Password too short', t => {
-  const lengthResult = p4g4rm3.validateLength('1234567')
+const password64 = 'eightlen'.repeat(8)
+const password65 = 'eightlen'.repeat(8).concat('1')
+
+test('Password too short', (t) => {
+  const lengthResult = p4g4rm3.validateMinimumLength('1234567')
   t.is(lengthResult, false)
 })
 
-test('Password containing valid length', t => {
-  const lengthResult = p4g4rm3.validateLength('12345678')
+test('Password containing valid minimum length', (t) => {
+  const lengthResult = p4g4rm3.validateMinimumLength('12345678')
+  t.is(lengthResult, true)
+})
+
+test('Password too long', (t) => {
+  const lengthResult = p4g4rm3.validateMaximumLength(password65)
+  t.is(lengthResult, false)
+})
+
+test('Password containing valid maximum length', (t) => {
+  const lengthResult = p4g4rm3.validateMaximumLength(password64)
   t.is(lengthResult, true)
 })
