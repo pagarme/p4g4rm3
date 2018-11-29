8ec96433486179e0c845f29fd6f1bed4e6930ee8
Dyego Lima <dyego9444@gmail.com>
2018-11-29 18:14:00 -0200

    validation: add password minimum length

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const zxcvbn = require('zxcvbn')
 
+const calculatePasswordScore = password => zxcvbn(password)
 
-const calculatePasswordScore = (password) => zxcvbn(password)
+const validateLength = (password) => {
+  if (password.length >= 8) {
+    return true
+  }
 
+  return false
+}
 
 module.exports = {
   calculatePasswordScore,
+  validateLength,
 }
