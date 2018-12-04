32a95da870220446554be18bf2647bce4dfe3575
Dyego Rodrigo <dyego9444@gmail.com>
2018-12-04 16:52:49 -0200

    validation: add validate function
    create validate function that will return an object containing all
    necessary verifications

--- test/unit/calculate-password-score.js
+++ test/unit/calculate-password-score.js
@@ -1,32 +1,27 @@
-import test from 'ava';
+import test from 'ava'
 import p4g4rm3 from '../../.'
 
-test('Too guessable password zxcvbn calculation', t => {
-  const passwordResult = p4g4rm3.calculatePasswordScore('123')
-  t.is(passwordResult.guesses, 13)
-  t.is(passwordResult.score, 0)
-});
+test('Too guessable password zxcvbn calculation', (t) => {
+  const { score: scoreResult } = p4g4rm3.validate('123')
+  t.is(scoreResult, 0)
+})
 
-test('Very guessable password zxcvbn calculation', t => {
-  const passwordResult = p4g4rm3.calculatePasswordScore('weakpass')
-  t.is(passwordResult.guesses, 161158)
-  t.is(passwordResult.score, 1)
-});
+test('Very guessable password zxcvbn calculation', (t) => {
+  const { score: scoreResult } = p4g4rm3.validate('weakpass')
+  t.is(scoreResult, 1)
+})
 
-test('Somewhat unguessable password zxcvbn calculation', t => {
-  const passwordResult = p4g4rm3.calculatePasswordScore('weakpass12')
-  t.is(passwordResult.guesses, 19063342)
-  t.is(passwordResult.score, 2)
-});
+test('Somewhat unguessable password zxcvbn calculation', (t) => {
+  const { score: scoreResult } = p4g4rm3.validate('weakpass12')
+  t.is(scoreResult, 2)
+})
 
-test('Safely unguessable password zxcvbn calculation', t => {
-  const passwordResult = p4g4rm3.calculatePasswordScore('kInd 0k3y')
-  t.is(passwordResult.guesses, 118410000)
-  t.is(passwordResult.score, 3)
-});
+test('Safely unguessable password zxcvbn calculation', (t) => {
+  const { score: scoreResult } = p4g4rm3.validate('kInd 0k3y')
+  t.is(scoreResult, 3)
+})
 
-test('Very unguessable password zxcvbn calculation', t => {
-  const passwordResult = p4g4rm3.calculatePasswordScore('He!! 0f A p@A55w0rd')
-  t.is(passwordResult.guesses, 48864000000010000)
-  t.is(passwordResult.score, 4)
-});
+test('Very unguessable password zxcvbn calculation', (t) => {
+  const { score: scoreResult } = p4g4rm3.validate('He!! 0f A p@A55w0rd')
+  t.is(scoreResult, 4)
+})
--- test/unit/is-commom-password.js
+++ test/unit/is-commom-password.js
@@ -1,12 +1,12 @@
-import test from 'ava';
+import test from 'ava'
 import p4g4rm3 from '../../.'
 
-test('Validate a commom password', t => {
+test('Validate a commom password', (t) => {
   const commomPassResult = p4g4rm3.isCommomPassword('vjjykgdjzbp')
   t.is(commomPassResult, true)
-});
+})
 
-test('Validate a uncommom password', t => {
+test('Validate a uncommom password', (t) => {
   const commomPassResult = p4g4rm3.isCommomPassword('you wi!! never c@tch meeee333')
   t.is(commomPassResult, false)
-});
+})
